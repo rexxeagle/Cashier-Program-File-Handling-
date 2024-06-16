7df447bd0cc4c752cc03456659fc18669026d80b
rendyelang <lsmnrndy123@gmail.com>
2024-06-16 13:15:19 +0700

    clear useless comment on write_invoice

--- file_handling/write_invoice.py
+++ file_handling/write_invoice.py
@@ -2,26 +2,6 @@ from datetime import datetime
 
 now = datetime.now()
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-# print(current_time)
-
-# def write(item, price, qty, cus_money):
-#     text = f'''
-#                  Eagle Tech Mart
-# -------------------------------------------------
-# Order Time: {current_time}   INV{now.strftime("%Y%S")}ETM
-# -------------------------------------------------
-# Item: {item}    Qty: {qty}    Price: Rp. {price:,.2f}
-#                    ------------------------------
-#                    Total Price: Rp. {price * qty:,.2f}
-#                 Customer Money: Rp. {cus_money:,.2f}
-#                         Change: Rp. {cus_money - (price * qty):,.2f}
-# -------------------------------------------------
-#           Thanks for shopping with us!
-#              Made by Rendy Elang
-# '''
-
-#     with open("invoices_output/invoice.txt", 'w') as invoice_file:
-#         invoice_file.write(text)
 
 def write(paid_items, cus_money):
     total_price = 0
